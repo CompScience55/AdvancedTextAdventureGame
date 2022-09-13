f3596e3a6fca28ec7be59a0105b3578a254d8e76
Tim Neumüllergit config --global user.email tim.neumueller1901@gmail.comgit initls -agit statusgit untouch *git statuscd C://Users//tneumull <tim.neumueller1901@gmail.com>
2022-09-13 19:51:51 +0200

    test commit

--- src/com/compScience/Main.java
+++ src/com/compScience/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args)  {
         AdventureGame adventureGame = new AdventureGame();
         adventureGame.startGame();
+        //TEst
     }
 }
